4ed9c82d519245773cbb5aec5b304010a902ce69
Rasmus Wiuff <rwiuff@gmail.com>
2025-01-13 16:01:37 +0100

    bla

--- src/main/java/dk/dtu/gravitrips/client/Client.java
+++ src/main/java/dk/dtu/gravitrips/client/Client.java
@@ -1,7 +1,15 @@
 package dk.dtu.gravitrips.client;
 
-public class Client {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class Client extends Application{
     public static void main(String[] args) {
-        
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
     }
 }
